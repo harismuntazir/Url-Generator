bec2cd2a376e6de37d2ec3064dde989a7adff5b7
harismuntazir <haris.0813.m@gmail.com>
2021-01-17 10:07:40 +0530

    Add files via upload

---
+++ generate.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hm
+ * Date: 7/24/2020
+ * Time: 2:41 PM
+ */
+
+?>
+<html lang="en">
+<head>
+    <title>Url Generator - Bad Tools</title>
+    <link rel="stylesheet" href="../styles/style.css"/>
+    <link rel="stylesheet" href="styles.css"/>
+
+</head>
+<body>
+<section id="main">
+    <section id="header">
+        <section id="logo">
+            <a href="https://errorworld.in/"><img src="../content/images/logos/logo.jpg" class="logo"/></a>
+        </section>
+        <?php require_once "../header.php" ?>
+    </section>
+
+    <?php
+//html
+require_once "../menu.php";
+//take user input
+
+$mainUrl = $_POST['mainUrl'];
+$parmStart = $_POST['parmStart'];
+$parmEnd = $_POST['parmEnd'];
+$extraUrl = $_POST['extraUrl'];
+@$isLeadingChar = $_POST['isLeadingChar'];
+@$leadingChar = $_POST['leadingChar'];
+@$leadCount = $_POST['leadCount'];
+
+$fp = fopen("Url list.rtf", "wb");
+
+echo '<section id="main_body">';
+echo '<section id="subtitle" style="margin-bottom: 1%">Sample of Generated Url List</section>';
+echo '<section id="sampleList">';
+if (!$isLeadingChar) {
+    for($i=$parmStart; $i <= $parmEnd; $i++) {
+        $url = $mainUrl . $i . $extraUrl . "\n";
+        if ($i < 15)
+            echo $url . "<br />";
+        @fwrite($fp, $url);
+    }
+}
+else {
+    $lead = "";
+    for ($j=$leadCount; $j>0;$j--) {
+        $lead .= $leadingChar;
+    }
+    for($i=$parmStart; $i <= $parmEnd; $i++) {
+        $url = $mainUrl . $lead . $i . $extraUrl . "\n";
+        if ($i < 15)
+            echo $url . "<br />";
+        @fwrite($fp, $url);
+    }
+}
+echo '</section>';
+echo "<section id='start'>";
+    echo "<a href='Url%20list.rtf'><button style='margin: 2% 0 1%' class='start'>Download</button></a>";
+    echo ucwords("<br /> note: you can rename the file to .txt format.");
+echo "</section>";
+echo "</section>";
+require_once "../footer.php";
---
+++ index.php
@@ -0,0 +1,93 @@
+<DOCTYPE html>
+
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: hm
+     * Date: 7/13/2020
+     * Time: 8:53 AM
+     */
+
+    ?>
+    <html lang="en">
+    <head>
+        <title>Url Generator - Bad Tools</title>
+        <link rel="stylesheet" href="../styles/style.css"/>
+        <link rel="stylesheet" href="styles.css"/>
+        <script type="text/javascript">
+            function decideLeading(isChecked) {
+                const leading = document.getElementById("leading");
+                const leading2 = document.getElementById("leading2");
+                if (isChecked.checked) {
+                    leading.disabled = false;
+                    leading2.disabled = false;
+                }
+                else {
+                    leading.disabled = true;
+                    leading2.disabled = true;
+                }
+            }
+        </script>
+
+    </head>
+    <body>
+    <section id="main">
+        <section id="header">
+            <section id="logo">
+                <a href="https://errorworld.in/"><img src="../content/images/logos/logo.jpg" class="logo"/></a>
+            </section>
+            <?php require_once "../header.php" ?>
+        </section>
+
+        <?php require_once "../menu.php" ?>
+
+        <section id="main_body">
+            <form action="generate.php" method="post">
+                <section id="urlGenForm">
+                    <label for="url" style="float: left; margin-top: 1%">Main Url</label>
+                    <input id="url" name="mainUrl" class="MainUrlBox" value="https://example.com/" required="required" type="text"/>
+                </section>
+
+                <section id="parm">
+                    <section style="float: left; margin-top: 6%;"><label for="parm">Start/End</label></section>
+                    <section style="display: inline-block">
+                        <section id="parmstart">
+                            <input value="1" required="required" name="parmStart" type="text" class="MainUrlBox"/>
+                        </section>
+                        <section id="parmend">
+                            <input value="50" required="required" name="parmEnd" type="text" class="MainUrlBox"/>
+                        </section>
+                    </section>
+                </section>
+
+                <section id="extraUrl">
+                    <label for="extUrl"></label><input name="extraUrl" type="text" value="ExtraURL" class="MainUrlBox extraUrl"/>
+                </section>
+
+                <section id="CheckBox">
+                        <input type="checkbox" class="checkBox" name="isLeadingChar" id="isChecked" onclick="decideLeading(this)"/>
+                        <label for="CheckBox">Leading Char</label>
+                </section>
+
+                <section id="CharLead">
+                    <section id="charfield">
+                        <label for="leading">Char</label>
+                        <input type="text" name="leadingChar" id="leading" class="MainUrlBox extraUrl" disabled="disabled"/>
+                    </section>
+                    <section id="count">
+                        <label for="leading2">Count</label>
+                        <input type="text" name="leadCount" id="leading2" class="MainUrlBox extraUrl" disabled="disabled"/>
+                    </section>
+                </section>
+
+                <section id="start">
+                    <button class="start">Start</button>
+                </section>
+            </form>
+        </section>
+
+        <?php require_once "../footer.php" ?>
+    </section>
+    </body>
+    </html>
+
